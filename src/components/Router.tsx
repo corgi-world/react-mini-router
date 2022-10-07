ec4fe315c9e022d7f916e45efd60c290c2a225fe
corgi-world <developer.msw@gmail.com>
2022-10-07 14:44:49 +0900

    feat: path에 맞는 child를 render하는 Router 구현

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,5 +1,21 @@
 import React from "react";
 
-export default function Router() {
-  return <div>Router</div>;
+interface RouterProps {
+  children: React.ReactNode;
+}
+
+export default function Router({ children }: RouterProps) {
+  const target = React.Children.map(children, (child) => {
+    if (React.isValidElement<{ path: string }>(child)) {
+      const {
+        props: { path },
+      } = child;
+
+      if (path === window.location.pathname) {
+        return child;
+      }
+    }
+  });
+
+  return <div>{target}</div>;
 }
