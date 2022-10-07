2888defacfdae5a5aaa5ebeb1d01865254aded67
corgi-world <developer.msw@gmail.com>
2022-10-07 18:33:50 +0900

    feat: 각 페이지에서 버튼 클릭 시 push

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import useRouter from "../../hooks/useRouter";
 
-export default function index() {
-  return <div>about</div>;
+export default function About() {
+  const { push } = useRouter();
+  return (
+    <div>
+      <h1>About</h1>
+      <button
+        onClick={() => {
+          push("/");
+        }}
+      >
+        root로 이동
+      </button>
+    </div>
+  );
 }
